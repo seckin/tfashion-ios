71508509727f60313025b687a46f87e449a976a2
Seckin Can Sahin <seckincansahin@gmail.com>
2015-06-20 15:45:08 +0300

    sending parse photos to conceive.io as they get uploaded

--- parse/cloud/photo.js
+++ parse/cloud/photo.js
@@ -11,3 +11,23 @@ Parse.Cloud.beforeSave('Photo', function(request, response) {
     response.error('Cannot set user on Photo to a user other than the current user.');
   }
 });
+
+
+Parse.Cloud.afterSave("Photo", function(request) {
+	Parse.Cloud.httpRequest({
+	  method: 'POST',
+	  url: 'http://conceive.io/create_media_from_parse',
+	  followRedirects: true,
+	  headers: {
+	    'Content-Type': 'application/json;charset=utf-8'
+	  },
+	  body: {
+	  	parse_link: "http://cdn.playbuzz.com/cdn/164dc032-b7eb-4e6d-8487-b1d9b2883273/97438fda-c723-449b-8f50-ff1962895ad4.jpg",
+	  	parse_object_id: request.object.id
+	  }
+	}).then(function(httpResponse) {
+	  console.log(httpResponse.text);
+	}, function(httpResponse) {
+	  console.error('Request failed with response code ' + httpResponse.status);
+	});
+});
