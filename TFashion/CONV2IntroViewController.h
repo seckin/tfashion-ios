cfb8945e46f19ffbe0f66ca8de3cfc1709923e00
Seckin Can Sahin <seckincansahin@gmail.com>
2015-06-28 01:18:58 +0300

    adding CONV2IntroViewController

---
+++ TFashion/CONV2IntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  CONV2IntroViewController.h
+//  TFashion
+//
+//  Created by Seckin Can Sahin on 6/28/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CONV2IntroViewController : UIViewController
+
+@end
---
+++ TFashion/CONV2IntroViewController.m
@@ -0,0 +1,37 @@
+//
+//  CONV2IntroViewController.m
+//  TFashion
+//
+//  Created by Seckin Can Sahin on 6/28/15.
+//
+//
+
+#import "CONV2IntroViewController.h"
+
+@interface CONV2IntroViewController ()
+
+@end
+
+@implementation CONV2IntroViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
