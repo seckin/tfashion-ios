a084c4ba8986974bda8eb0f7ad3df2bf451b8d0e
Seckin Can Sahin <seckincansahin@gmail.com>
2015-07-02 21:29:29 +0300

    making the separators white in follow views

--- TFashion/CONFollowersViewController.m
+++ TFashion/CONFollowersViewController.m
@@ -76,7 +76,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    self.tableView.separatorColor = [UIColor whiteColor];//[UIColor colorWithRed:30.0f/255.0f green:30.0f/255.0f blue:30.0f/255.0f alpha:1.0f];
+    self.tableView.separatorColor = [UIColor whiteColor];
 }
 
 #pragma mark - UITableViewDelegate
